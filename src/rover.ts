09dc87f265fc3911f242b19db2080373dce9a66d
Chris Harding <chris.harding@cazoo.co.uk>
2021-04-13 14:49:07 +0100

    rover rotates right

--- src/rover.ts
+++ src/rover.ts
@@ -5,8 +5,10 @@ export class Rover {
             return "0:0:W";
         } else if(input == 'RR') {
             return "0:0:S";
+        } else if(input == 'R') {
+            return "0:0:E";
         }
 
-        return "0:0:E";
+        return "0:0:N";
     }
 }
--- src/test/mars-rover.test.ts
+++ src/test/mars-rover.test.ts
@@ -1,16 +1,18 @@
 import { Rover } from "../rover";
 
 describe('Mars Rover', () => {
-    it.each([
-        ["", "0:0:N"],
-        ["R", "0:0:E"],
-        ["RR", "0:0:S"],
-        ["RRR", "0:0:W"],
-    ])('should not move or rotate without commands', (input, expectedPosition) => {
-        const rover = new Rover();
-
-        const newPosition = rover.command(input);
-
-        expect(newPosition).toEqual(expectedPosition);
-    });
+    describe('rotating right', () => {
+        it.each([
+            ["", "0:0:N"],
+            ["R", "0:0:E"],
+            ["RR", "0:0:S"],
+            ["RRR", "0:0:W"],
+        ])('with input %s should return %s', (input, expectedPosition) => {
+            const rover = new Rover();
+    
+            const newPosition = rover.command(input);
+    
+            expect(newPosition).toEqual(expectedPosition);
+        });
+    })
 });
