c195d5f5861d698d183db3e93890aa71ed792160
Chris Harding <chris.harding@cazoo.co.uk>
2021-04-13 14:35:40 +0100

    mob next [ci-skip]

---
+++ src/rover.ts
@@ -0,0 +1,5 @@
+export class Rover {
+    command(input: string): string {
+        return "0:0:E";
+    }
+}
--- src/test/mars-rover.test.ts
+++ src/test/mars-rover.test.ts
@@ -1,3 +1,5 @@
+import { Rover } from "../rover";
+
 describe('Mars Rover', () => {
     it('should turn from North to East', () => {
         const rover = new Rover();
@@ -5,5 +7,13 @@ describe('Mars Rover', () => {
         const newPosition = rover.command("R");
 
         expect(newPosition).toEqual("0:0:E")
+    });
+
+    it('should turn from North to East to South', () => {
+        const rover = new Rover();
+
+        const newPosition = rover.command("RR");
+
+        expect(newPosition).toEqual("0:0:S")
     });
 });
